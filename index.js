b7338a80831c2dddd9cb84bde7932e06987c4886
blakeskipper <blakeskipper@gmail.com>
2022-03-18 11:54:33 -0400

    commit tree 75eefbc207a8258ced2ea7ce36ee47033a5fae3e

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solutions in this file
+function countDown(positiveNumber) {
+    while(positiveNumber >= 0){
+        console.log(positiveNumber);
+        positiveNumber--;
+    }
+}
+
+
+
+
+
+
+const names = ["Blake", "Jeffrey", "Trevor"];
+
+function writeCards(names, event){
+    let messages = [];
+    for(let i = 0; i < names.length; i++){
+messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+    }
+    return messages;
+}
+
+
+
+
